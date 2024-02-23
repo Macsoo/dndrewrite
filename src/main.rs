e75a53fdc94926230a42f105dd68db612f66539a
Tibor File <filetibor2002@gmail.com>
2024-02-23 18:12:56 +0100

    Creates two windows at startup
    Also modifies current systems to work with multiple windows.

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,18 @@ use std::ops::Deref;
 use bevy::asset::{LoadedFolder, LoadState};
 use bevy::prelude::*;
 use bevy::input::mouse::MouseWheel;
+use bevy::render::camera::RenderTarget;
 use bevy::render::mesh::{Indices, PrimitiveTopology};
 use bevy::sprite::MaterialMesh2dBundle;
 use bevy::utils::HashMap;
+use bevy::window::{ExitCondition, WindowFocused, WindowRef};
 use hexx::{Hex, HexLayout, HexOrientation, PlaneMeshBuilder, shapes};
 
 #[derive(States, PartialEq, Eq, Debug, Clone, Hash, Default)]
 enum AppStates {
     #[default]
     Loading,
-    Loaded
+    Loaded,
 }
 
 #[derive(Resource, Default)]
@@ -57,23 +59,23 @@ impl Textures {
         tile_colors.insert(tile_color.into_owned(), handles);
     }
 
-    fn get_tile_types(&self) -> impl Iterator<Item = &str> {
+    fn get_tile_types(&self) -> impl Iterator<Item=&str> {
         self.tile_textures.keys().map(String::as_str)
     }
 
-    fn get_tile_variants(&self, tile_type: &str) -> Option<impl Iterator<Item = &str>> {
-        self.tile_textures.get(tile_type).map(HashMap::keys).map(|x|x.map(String::as_str))
+    fn get_tile_variants(&self, tile_type: &str) -> Option<impl Iterator<Item=&str>> {
+        self.tile_textures.get(tile_type).map(HashMap::keys).map(|x| x.map(String::as_str))
     }
 
     fn get_tile_colors(
         &self,
         tile_type: &str,
         tile_variants: &str,
-    ) -> Option<impl Iterator<Item = &str>> {
+    ) -> Option<impl Iterator<Item=&str>> {
         self.tile_textures.get(tile_type)
             .and_then(|hm| hm.get(tile_variants))
             .map(HashMap::keys)
-            .map(|x|x.map(String::as_str))
+            .map(|x| x.map(String::as_str))
     }
 
     fn get_tile_textures(
@@ -134,13 +136,13 @@ fn check_loading(
     mut next_state: ResMut<NextState<AppStates>>,
 ) {
     for ev in asset_event.read() {
-        let AssetEvent::LoadedWithDependencies { id } = ev else { continue };
+        let AssetEvent::LoadedWithDependencies { id } = ev else { continue; };
         let loaded_folder = loaded_folders.remove(*id).unwrap();
         let (name, _) = folders_loading.remove(id);
         info!("Loaded folder '{}'.", name);
         if let Some(name) = name.strip_prefix("pointy.") {
-            let Some((tile_name, color)) = name.split_once('.') else { continue };
-            let Some((tile_type, tile_variant)) = tile_name.rsplit_once('_') else { continue };
+            let Some((tile_name, color)) = name.split_once('.') else { continue; };
+            let Some((tile_type, tile_variant)) = tile_name.rsplit_once('_') else { continue; };
             let handles: Vec<Handle<Image>> = loaded_folder.handles
                 .into_iter()
                 .map(UntypedHandle::typed)
@@ -148,17 +150,17 @@ fn check_loading(
             textures.push_tile(tile_type.into(), tile_variant.into(), color.into(), handles);
         } else {
             let Some(name) = name.strip_prefix("overlay_")
-                .and_then(|s| s.strip_suffix(".standard_full")) else { continue };
+                .and_then(|s| s.strip_suffix(".standard_full")) else { continue; };
             let mut handles: Vec<Handle<Image>> = loaded_folder.handles.into_iter()
                 .map(|h| h.typed())
                 .collect();
             handles.sort_by_cached_key(|h| {
-                let Some(asset_path) = h.path() else { return u32::MAX };
-                let Some(file_name_os) = asset_path.path().file_name() else { return u32::MAX };
-                let Some(file_name) = file_name_os.to_str() else { return u32::MAX };
-                let Some(without_extension) = file_name.strip_suffix(".png") else { return u32::MAX };
-                let Some(split) = without_extension.rsplit_once('_') else { return u32::MAX };
-                let Ok(index) = split.1.parse::<u32>() else { return u32::MAX };
+                let Some(asset_path) = h.path() else { return u32::MAX; };
+                let Some(file_name_os) = asset_path.path().file_name() else { return u32::MAX; };
+                let Some(file_name) = file_name_os.to_str() else { return u32::MAX; };
+                let Some(without_extension) = file_name.strip_suffix(".png") else { return u32::MAX; };
+                let Some(split) = without_extension.rsplit_once('_') else { return u32::MAX; };
+                let Ok(index) = split.1.parse::<u32>() else { return u32::MAX; };
                 index
             });
             if name == "figures" {
@@ -177,6 +179,28 @@ fn setup(
     mut commands: Commands,
     textures: Res<Textures>,
 ) {
+    let admin_window = commands.spawn(Window {
+        title: String::from("Faerûn - Admin"),
+        ..default()
+    }).id();
+    let admin_camera = commands.spawn(Camera2dBundle {
+        camera: Camera {
+            target: RenderTarget::Window(WindowRef::Entity(admin_window)),
+            ..default()
+        },
+        ..default()
+    }).id();
+    let user_window = commands.spawn(Window {
+        title: String::from("Faerûn"),
+        ..default()
+    }).id();
+    let user_camera = commands.spawn(Camera2dBundle {
+        camera: Camera {
+            target: RenderTarget::Window(WindowRef::Entity(user_window)),
+            ..default()
+        },
+        ..default()
+    }).id();
     let layout = HexLayout {
         hex_size: Vec2::splat(105. * 3f32.sqrt()),
         orientation: HexOrientation::Pointy,
@@ -195,46 +219,68 @@ fn setup(
             });
         })
         .collect::<Vec<_>>();
-    commands.spawn(Camera2dBundle::default());
+}
+
+#[derive(Resource, Default)]
+struct Focus(Option<Entity>);
+
+fn focus_checker(
+    mut event_reader: EventReader<WindowFocused>,
+    mut focus: ResMut<Focus>,
+) {
+    for ev in event_reader.read() {
+        if !ev.focused { continue }
+        focus.0 = Some(ev.window);
+    }
 }
 
 fn move_camera(
-    mut camera: Query<&mut Transform, With<Camera>>,
+    mut cameras: Query<(&mut Transform, &Camera)>,
     time: Res<Time>,
+    focus: Res<Focus>,
     keys: Res<Input<KeyCode>>,
 ) {
-    let mut camera = camera.single_mut();
-    let vel = 10_000. * time.delta().as_secs_f32();
-    if keys.pressed(KeyCode::W) {
-        camera.translation.y += vel;
-    }
-    if keys.pressed(KeyCode::A) {
-        camera.translation.x -= vel;
-    }
-    if keys.pressed(KeyCode::S) {
-        camera.translation.y -= vel;
-    }
-    if keys.pressed(KeyCode::D) {
-        camera.translation.x += vel;
+    for (mut transform, camera) in cameras.iter_mut() {
+        let RenderTarget::Window(window) = camera.target else { continue; };
+        let WindowRef::Entity(entity) = window else { continue; };
+        if focus.0 != Some(entity) { continue; }
+        let vel = 10_000. * time.delta().as_secs_f32();
+        if keys.pressed(KeyCode::W) {
+            transform.translation.y += vel;
+        }
+        if keys.pressed(KeyCode::A) {
+            transform.translation.x -= vel;
+        }
+        if keys.pressed(KeyCode::S) {
+            transform.translation.y -= vel;
+        }
+        if keys.pressed(KeyCode::D) {
+            transform.translation.x += vel;
+        }
     }
 }
 
 fn zoom(
-    mut camera: Query<&mut OrthographicProjection, With<Camera>>,
+    mut cameras: Query<(&mut OrthographicProjection, &Camera)>,
     time: Res<Time>,
+    focus: Res<Focus>,
     mut scale: ResMut<Scale>,
     mut scroll: EventReader<MouseWheel>,
 ) {
     use bevy::input::mouse::MouseScrollUnit;
-    let mut camera = camera.single_mut();
     for ev in scroll.read() {
-        match ev.unit {
-            MouseScrollUnit::Line => {
-                scale.0 -= time.delta().as_secs_f32() * ev.y;
-                scale.0 = scale.0.clamp(-2., 4.);
-                camera.scale = scale.0.exp();
+        for (mut proj, camera) in cameras.iter_mut() {
+            let RenderTarget::Window(window) = camera.target else { continue };
+            let WindowRef::Entity(entity) = window else { continue };
+            if focus.0 != Some(entity) { continue }
+            match ev.unit {
+                MouseScrollUnit::Line => {
+                    scale.0 -= time.delta().as_secs_f32() * ev.y;
+                    scale.0 = scale.0.clamp(-2., 4.);
+                    proj.scale = scale.0.exp();
+                }
+                MouseScrollUnit::Pixel => {}
             }
-            MouseScrollUnit::Pixel => {}
         }
     }
 }
@@ -242,17 +288,22 @@ fn zoom(
 fn map_drag(
     mut mouse_movement: EventReader<CursorMoved>,
     mouse_button: Res<Input<MouseButton>>,
-    mut camera: Query<&mut Transform, With<Camera>>,
+    mut cameras: Query<(&mut Transform, &Camera)>,
     mut last_pos: ResMut<MouseLastPosition>,
+    focus: Res<Focus>,
     scale: Res<Scale>,
 ) {
-    let mut camera = camera.single_mut();
     for ev in mouse_movement.read() {
-        if mouse_button.pressed(MouseButton::Left) {
-            let d = ev.position - last_pos.0;
-            let d = d * scale.0.exp();
-            camera.translation += Vec3::new(-d.x, d.y, 0.);
-            last_pos.0 = ev.position;
+        for (mut transform, camera) in cameras.iter_mut() {
+            let RenderTarget::Window(window) = camera.target else { continue };
+            let WindowRef::Entity(entity) = window else { continue };
+            if focus.0 != Some(entity) { continue }
+            if mouse_button.pressed(MouseButton::Left) {
+                let d = ev.position - last_pos.0;
+                let d = d * scale.0.exp();
+                transform.translation += Vec3::new(-d.x, d.y, 0.);
+                last_pos.0 = ev.position;
+            }
         }
     }
 }
@@ -260,23 +311,29 @@ fn map_drag(
 fn detect_press(
     mouse_button: Res<Input<MouseButton>>,
     mut last_pos: ResMut<MouseLastPosition>,
-    window: Query<&Window>,
+    windows: Query<&Window>,
 ) {
-    let window = window.single();
-    if mouse_button.just_pressed(MouseButton::Left) {
-        if let Some(pos) = window.cursor_position() {
-            last_pos.0 = pos;
+    for window in windows.iter() {
+        if mouse_button.just_pressed(MouseButton::Left) {
+            if let Some(pos) = window.cursor_position() {
+                last_pos.0 = pos;
+            }
         }
     }
 }
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: None,
+            close_when_requested: true,
+            exit_condition: ExitCondition::DontExit,
+        }))
         .init_resource::<Scale>()
         .init_resource::<MouseLastPosition>()
         .init_resource::<Textures>()
         .init_resource::<FoldersLoading>()
+        .init_resource::<Focus>()
         .add_state::<AppStates>()
         .add_systems(OnEnter(AppStates::Loading), load)
         .add_systems(Update, check_loading.run_if(in_state(AppStates::Loading)))
@@ -285,6 +342,11 @@ fn main() {
             move_camera,
             zoom,
             (detect_press, map_drag).chain(),
+        ).run_if(in_state(AppStates::Loaded)))
+        .add_systems(Update, (
+            bevy::window::close_on_esc,
+            bevy::window::exit_on_all_closed,
+            focus_checker,
         ).run_if(in_state(AppStates::Loaded)))
         .run();
 }
